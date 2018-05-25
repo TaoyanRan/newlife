b995f8a2b58aabf5a03c4c2563971e5cb45f89a5
TaoyanRan <2089715427@qq.com>
2018-05-25 10:19:09 +0800

    第一次改版，第一次提交

---
+++ js/rem.js
@@ -0,0 +1,34 @@
+(function(doc, win) {
+
+
+	var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function() {
+			var clientWidth = docEl.clientWidth;
+            console.log(clientWidth)
+			if(!clientWidth) return;
+			if(clientWidth >= 1280){
+                // docEl.style.fontSize = clientWidth / 13.66 + 'px';
+                // docEl.style.fontSize = clientWidth / 19.2 + 'px';
+                docEl.style.fontSize = '100px';
+
+            }else if(clientWidth < 640){
+				docEl.style.fontSize = clientWidth / 7.5 + 'px';
+			}
+
+
+			if(clientWidth <= 960){
+
+                var meta = document.getElementsByTagName('meta');
+                var scale = .4;
+                meta["viewport"].setAttribute('content',"width=device-width,initial-scale=" + scale + ",minimum-scale=" + scale + ",maximum-scale=" + scale + ",user-scalable=yes");
+            }
+
+
+		};
+
+	if(!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+	doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+			 
---
+++ js/utils.js
@@ -0,0 +1,58 @@
+// tab切换
+function changeTab(tabParentDom) {
+    $(tabParentDom + ' .tabHeader .tabItem').each(function (i) {
+        $(this).click(function () {
+            $(tabParentDom + ' .tabHeader .tabItem').removeClass('active').eq(i).addClass('active');
+            $(tabParentDom + ' .tabContainer .tabItem').removeClass('active').eq(i).addClass('active')
+        })
+    })
+};
+
+// 新闻列表 单轴两列
+function axisColumn_2() {
+    var newsList = [];
+
+    function createNewsList() {
+        var maxLength = $('.axisColumn-2 li').length;
+        var forLength = Math.floor((maxLength + 1) / 2);
+        for(var i=0;i<forLength;i++){
+            newsList.push(i);
+            var j = forLength + i;
+            if(j < maxLength){
+                newsList.push(j)
+            }
+        }
+    }
+    createNewsList();
+
+    var changeNresData = {
+        start : 0,
+        newsLength: newsList.length,
+        time: 3000
+    };
+
+    var CHANGENEWS = setInterval(changeNews,changeNresData.time);
+
+    function changeNews() {
+        changeNresData.start ++;
+        changeNresData.start  = changeNresData.start >= changeNresData.newsLength ? 0 : changeNresData.start;
+        var newsI = newsList[changeNresData.start];
+        $('.axisColumn-2 li').removeClass('active').eq(newsI).addClass('active');
+    }
+
+    $('.axisColumn-2 li').each(function (i) {
+        $(this).hover(function () {
+            $('.axisColumn-2 li').removeClass('active').eq(i).addClass('active');
+            changeNresData.start = i;
+        })
+    });
+
+    $('.axisColumn-2').on("mouseenter", function(){
+        clearInterval(CHANGENEWS);
+    });
+    $('.axisColumn-2').on("mouseleave", function(){
+        CHANGENEWS = setInterval(changeNews,changeNresData.time);
+    });
+}
+
+
